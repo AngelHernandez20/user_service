07cca658000267328139752e718b4004fd588e29
AngelHernandez20 <191280@ids.upchiapas.edu.mx>
2022-12-11 15:37:56 -0600

    archivo yml

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,6 @@ import * as loginCtrl from '../controllers/auth.controller.js'
 const router = Router()
 
 router.post('/login',loginCtrl.loginUser2)
-router.post('/signin')
+
 
 export default router;
